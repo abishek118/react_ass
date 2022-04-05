fe17578684b0c17661ec80b8e59bdf0847815fe5
Nayana S <69519075+Nayanasalur@users.noreply.github.com>
2022-04-05 13:21:53 +0530

    sub_assig_3

--- Assignment-3/code/src/App.js
+++ Assignment-3/code/src/App.js
@@ -2,13 +2,67 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const User = {
+  Name : 'Alan Ford',
+  EmployeeId : '00005152',
+  Appointment : '9:00 (24-05-2016)',
+  Email : 'alan.form@email.nl',
+  phone : '+31123456789',
+  profileImg:'https://www.w3schools.com/howto/img_avatar.png',
+  product_name: "Boltart Bosbessen",
+  product_description:
+    "This is some random description about the product mentioned above"
+}
 
 var arrow="<"
-function App() {
+var arrow1 = ">"
+function App(props) {
+  const {User} = props
+  const {Name,EmployeeId,Appointment,Email,phone,profileImg,product_name,product_description} = props.User
   return (
     <div className="site-container">
     {/* Start Your code here */}
+    <div className='header'>
+      <p id='header-para'>{arrow}</p>
+      <div className='header-mid'>
+      <h3>{Name}</h3>
+      <p style={{color:'silver'}}>{EmployeeId}</p>
+      </div>
+      <button>Print</button>
     </div>
+    <div className='customer-info'>
+      <h3>Appointment:{Appointment}</h3>
+      <h3>Email:{Email}</h3>
+      <h3>phone:{phone}</h3>
+    </div>
+    <div className='order-info'>
+      <div>
+        <h3>Status</h3>
+        <li>In Progress</li>
+      </div>
+      <div id='order1'>
+        <h3>Door</h3>
+        <p className='order-para'>Mark</p>
+      </div>
+      <div className='order-time'>
+        <h3>Time</h3>
+        <p id='time1'>10:30 (25-05-2016)</p>
+      </div>
+    </div>
+    <div className='product-list'>
+      <div className='checkbox'>
+          <input type={'checkbox'}></input>
+      </div>
+      <div>
+        <img src={profileImg}></img>
+      </div>
+      <div className='name'>
+        <h3>{product_name}</h3>
+        <p>{product_description}</p>
+      </div>
+      <div className='arr'>{arrow1}</div>
+    </div>
+  </div>
   );
 }
 
--- Assignment-3/code/src/index.js
+++ Assignment-3/code/src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+const data = {
+  Name : 'Alan Ford',
+  EmployeeId : '00005152',
+  Appointment : '9:00 (24-05-2016)',
+  Email : 'alan.form@email.nl',
+  phone : '+31123456789',
+  profileImg:'https://www.w3schools.com/howto/img_avatar.png',
+  product_name: "Boltart Bosbessen",
+  product_description:
+    "This is some random description about the product mentioned above"
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App User={data}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
